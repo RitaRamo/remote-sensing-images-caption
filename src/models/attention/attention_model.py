8d1e63c6a5eb782f93ded1b957f86afe652adb7b
RitaRamos <rita.mparada.ramos@gmail.com>
2020-03-09 10:50:44 +0000

    simplified attention model

--- src/models/attention/attention_model.py
+++ src/models/attention/attention_model.py
@@ -1,59 +1,10 @@
 import torchvision
 from torch import nn
 import torch
-torch.manual_seed(0)
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-
-class Encoder(nn.Module):
-    """
-    Encoder.
-    """
-
-    def __init__(self, encoded_image_size=14):
-        super(Encoder, self).__init__()
-        self.enc_image_size = encoded_image_size
-
-        # TODO: mudar isto
-        resnet = torchvision.models.resnet101(
-            pretrained=True)  # pretrained ImageNet ResNet-101
-
-        # Remove linear and pool layers (since we're not doing classification)
-        modules = list(resnet.children())[:-2]
-        self.resnet = nn.Sequential(*modules)
-
-        # Resize image to fixed size to allow input images of variable size
-        self.adaptive_pool = nn.AdaptiveAvgPool2d(
-            (encoded_image_size, encoded_image_size))
-
-        self.fine_tune()
-
-    def forward(self, images):
-        """
-        Forward propagation.
-        :param images: images, a tensor of dimensions (batch_size, 3, image_size, image_size)
-        :return: encoded images
-        """
-        out = self.resnet(
-            images)  # (batch_size, 2048, image_size/32, image_size/32)
-        # (batch_size, 2048, encoded_image_size, encoded_image_size)
-        out = self.adaptive_pool(out)
-        # (batch_size, encoded_image_size, encoded_image_size, 2048)
-        out = out.permute(0, 2, 3, 1)
-        return out
-
-    def fine_tune(self, fine_tune=True):
-        """
-        Allow or prevent the computation of gradients for convolutional blocks 2 through 4 of the encoder.
-        :param fine_tune: Allow?
-        """
-        for p in self.resnet.parameters():
-            p.requires_grad = False
-        # If fine-tuning, only fine-tune convolutional blocks 2 through 4
-        for c in list(self.resnet.children())[5:]:
-            for p in c.parameters():
-                p.requires_grad = fine_tune
+from torch.nn.utils.rnn import pack_padded_sequence
+from models.basic_model import BasicModel, Encoder
+import torch.nn.functional as F
+from embeddings.embeddings import get_embedding_layer
 
 
 class Attention(nn.Module):
@@ -96,12 +47,12 @@ class Attention(nn.Module):
         return attention_weighted_encoding, alpha
 
 
-class DecoderWithAttention(nn.Module):
+class Decoder(nn.Module):
     """
     Decoder.
     """
 
-    def __init__(self, attention_dim, embed_dim, decoder_dim, vocab_size, encoder_dim=2048, dropout=0.5):
+    def __init__(self, attention_dim, embedding_type, embed_dim, decoder_dim, vocab_size, token_to_id, encoder_dim=2048, dropout=0.5):
         """
         :param attention_dim: size of attention network
         :param embed_dim: embedding size
@@ -110,7 +61,7 @@ class DecoderWithAttention(nn.Module):
         :param encoder_dim: feature size of encoded images
         :param dropout: dropout
         """
-        super(DecoderWithAttention, self).__init__()
+        super(Decoder, self).__init__()
 
         self.encoder_dim = encoder_dim
         self.attention_dim = attention_dim
@@ -122,7 +73,10 @@ class DecoderWithAttention(nn.Module):
         self.attention = Attention(
             encoder_dim, decoder_dim, attention_dim)  # attention network
 
-        self.embedding = nn.Embedding(vocab_size, embed_dim)  # embedding layer
+        # self.embedding = nn.Embedding(vocab_size, embed_dim)  # embedding layer
+        self.embedding = get_embedding_layer(
+            embedding_type, embed_dim, vocab_size, token_to_id)
+
         self.dropout = nn.Dropout(p=self.dropout)
         self.decode_step = nn.LSTMCell(
             embed_dim + encoder_dim, decoder_dim, bias=True)  # decoding LSTMCell
@@ -130,10 +84,7 @@ class DecoderWithAttention(nn.Module):
         self.init_h = nn.Linear(encoder_dim, decoder_dim)
         # linear layer to find initial cell state of LSTMCell
         self.init_c = nn.Linear(encoder_dim, decoder_dim)
-        # linear layer to create a sigmoid-activated gate
-        self.f_beta = nn.Linear(decoder_dim, encoder_dim)
-        self.sigmoid = nn.Sigmoid()
-        # linear layer to find scores over vocabulary
+
         self.fc = nn.Linear(decoder_dim, vocab_size)
         self.init_weights()  # initialize some layers with the uniform distribution
 
@@ -141,16 +92,11 @@ class DecoderWithAttention(nn.Module):
         """
         Initializes some parameters with values from the uniform distribution, for easier convergence.
         """
-        self.embedding.weight.data.uniform_(-0.1, 0.1)
+        #self.embedding.weight.data.uniform_(-0.1, 0.1)
+        #print("e agora", self.embedding.weight.data)
+
         self.fc.bias.data.fill_(0)
         self.fc.weight.data.uniform_(-0.1, 0.1)
-
-    def load_pretrained_embeddings(self, embeddings):
-        """
-        Loads embedding layer with pre-trained embeddings.
-        :param embeddings: pre-trained embeddings
-        """
-        self.embedding.weight = nn.Parameter(embeddings)
 
     def fine_tune_embeddings(self, fine_tune=True):
         """
@@ -171,75 +117,98 @@ class DecoderWithAttention(nn.Module):
         c = self.init_c(mean_encoder_out)
         return h, c
 
-    def forward(self, encoder_out, encoded_captions, caption_lengths):
-        """
-        Forward propagation.
-        :param encoder_out: encoded images, a tensor of dimension (batch_size, enc_image_size, enc_image_size, encoder_dim)
-        :param encoded_captions: encoded captions, a tensor of dimension (batch_size, max_caption_length)
-        :param caption_lengths: caption lengths, a tensor of dimension (batch_size, 1)
-        :return: scores for vocabulary, sorted encoded captions, decode lengths, weights, sort indices
-        """
+    def forward(self, word, encoder_out, decoder_hidden_state, decoder_cell_state):
 
+        attention_weighted_encoding, alpha = self.attention(encoder_out,
+                                                            decoder_hidden_state)
+
+        embeddings = self.embedding(word)
+
+        decoder_input = torch.cat(
+            (embeddings, attention_weighted_encoding), dim=1
+        )
+
+        decoder_hidden_state, decoder_cell_state = self.decode_step(
+            decoder_input, (decoder_hidden_state, decoder_cell_state)
+        )
+
+        scores = self.fc(self.dropout(decoder_hidden_state))
+
+        return scores, decoder_hidden_state, decoder_cell_state, alpha
+
+
+class AttentionModel(BasicModel):
+
+    MODEL_DIRECTORY = "experiments/results/attention_model/"
+
+    def __init__(self,
+                 args,
+                 vocab_size,
+                 token_to_id,
+                 id_to_token,
+                 max_len,
+                 device
+                 ):
+        super().__init__(args, vocab_size, token_to_id, id_to_token, max_len, device)
+
+    def _initialize_encoder_and_decoder(self):
+
+        self.decoder = Decoder(
+            attention_dim=self.args.attention_dim,
+            decoder_dim=self.args.decoder_dim,
+            embedding_type=self.args.embedding_type,
+            embed_dim=self.args.embed_dim,
+            vocab_size=self.vocab_size,
+            token_to_id=self.token_to_id,
+            dropout=self.args.dropout
+        )
+
+        self.encoder = Encoder(enable_fine_tuning=self.args.fine_tune_encoder)
+
+        self.decoder = self.decoder.to(self.device)
+        self.encoder = self.encoder.to(self.device)
+
+    def _calculate_loss(self, encoder_out, caps, caption_lengths):
         batch_size = encoder_out.size(0)
-        encoder_dim = encoder_out.size(-1)
-        vocab_size = self.vocab_size
-
-        # Flatten image
-        # (batch_size, num_pixels, encoder_dim)
-        encoder_out = encoder_out.view(batch_size, -1, encoder_dim)
         num_pixels = encoder_out.size(1)
 
-        # Sort input data by decreasing lengths; why? apparent below
-        caption_lengths, sort_ind = caption_lengths.squeeze(
-            1).sort(dim=0, descending=True)
-        print("\nthis is caption lenghts", caption_lengths, sort_ind)
-        encoder_out = encoder_out[sort_ind]
-        encoded_captions = encoded_captions[sort_ind]
-        print("\nencoded captions", encoded_captions)
-        # Embedding
-        # (batch_size, max_caption_length, embed_dim)
-        embeddings = self.embedding(encoded_captions)
-
-        # Initialize LSTM state
-        h, c = self.init_hidden_state(encoder_out)  # (batch_size, decoder_dim)
-
-        # We won't decode at the <end> position, since we've finished generating as soon as we generate <end>
-        # So, decoding lengths are actual lengths - 1
-        decode_lengths = (caption_lengths - 1).tolist()
-        print("\ndecoded lenghts", decode_lengths)
-
         # Create tensors to hold word predicion scores and alphas
-        predictions = torch.zeros(batch_size, max(
-            decode_lengths), vocab_size).to(device)
-        alphas = torch.zeros(batch_size, max(
-            decode_lengths), num_pixels).to(device)
+        all_predictions = torch.zeros(batch_size, max(
+            caption_lengths), self.vocab_size).to(self.device)
+        all_alphas = torch.zeros(batch_size, max(
+            caption_lengths), num_pixels).to(self.device)
 
-        # At each time-step, decode by
-        # attention-weighing the encoder's output based on the decoder's previous hidden state output
-        # then generate a new word in the decoder with the previous word and the attention weighted encoding
-        for t in range(max(decode_lengths)):
-            batch_size_t = sum([l > t for l in decode_lengths])
-            attention_weighted_encoding, alpha = self.attention(encoder_out[:batch_size_t],
-                                                                h[:batch_size_t])
-            # gating scalar, (batch_size_t, encoder_dim)
-            gate = self.sigmoid(self.f_beta(h[:batch_size_t]))
-            attention_weighted_encoding = gate * attention_weighted_encoding
-            h, c = self.decode_step(
-                torch.cat([embeddings[:batch_size_t, t, :],
-                           attention_weighted_encoding], dim=1),
-                (h[:batch_size_t], c[:batch_size_t]))  # (batch_size_t, decoder_dim)
-            preds = self.fc(self.dropout(h))  # (batch_size_t, vocab_size)
-            predictions[:batch_size_t, t, :] = preds
-            alphas[:batch_size_t, t, :] = alpha
+        h, c = self.decoder.init_hidden_state(encoder_out)
 
-        return predictions, encoded_captions, decode_lengths, alphas, sort_ind
+        # Predict
+        for t in range(max(
+                caption_lengths)):
+            # batchsizes of current time_step are the ones with lenght bigger than time-step (i.e have not fineshed yet)
+            batch_size_t = sum([l > t for l in caption_lengths])
 
+            predictions, h, c, alpha = self.decoder(
+                caps[:batch_size_t, t], encoder_out[:batch_size_t], h[:batch_size_t], c[:batch_size_t])
 
-# primeiro veres isto a funcar com sorted; depois com packed e depois sem nada
-# veres isto a funcar com packed
-# loss com e sem logsofma nnlg
-# com e se padding
+            all_predictions[:batch_size_t, t, :] = predictions
+            all_alphas[:batch_size_t, t, :] = alpha
 
+        # Calculate loss
+        targets = caps[:, 1:]  # targets doesnt have stark token
 
-# depois fazeres isto como no tensorflow_ o q é a loss + optimizer + etc...
-# por modelo encoder + decoder tmbm...
+        scores = pack_padded_sequence(
+            all_predictions, caption_lengths, batch_first=True)
+        targets = pack_padded_sequence(
+            targets, caption_lengths, batch_first=True)
+
+        loss = self.criterion(scores.data, targets.data)
+
+        return loss
+
+    def generate_output_index(self, input_word, encoder_out, h, c):
+        predictions, h, c, _ = self.decoder(
+            input_word, encoder_out, h, c)
+
+        scores = F.log_softmax(predictions, dim=1)
+        current_output_index = torch.argmax(scores, dim=1)
+
+        return current_output_index, h, c
