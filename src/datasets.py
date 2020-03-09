81b799769ef3ad1763dfdce57aff2f660d8f5f83
RitaRamos <rita.mparada.ramos@gmail.com>
2020-03-09 10:50:12 +0000

    remove target of datasets

--- src/datasets.py
+++ src/datasets.py
@@ -1,6 +1,5 @@
 import torch
 from torch.utils.data import Dataset
-import json
 from PIL import Image
 from preprocess_data.tokens import convert_captions_to_Y
 from create_data_files import get_dataset
@@ -23,7 +22,7 @@ class CaptionDataset(Dataset):
         self.images_names, captions_of_tokens = dataset[
             "images_names"], dataset["captions_tokens"]
 
-        self.input_captions, self.target_captions, self.captions_lengths = convert_captions_to_Y(
+        self.input_captions, self.captions_lengths = convert_captions_to_Y(
             captions_of_tokens, max_len, token_to_id)
 
         # logging.info("len dataset of %s is %s",
@@ -39,13 +38,57 @@ class CaptionDataset(Dataset):
         image = self.transform(image)
 
         input_caption = self.input_captions[i]
-        target_caption = self.target_captions[i]
         caption_lenght = self.captions_lengths[i]
 
-        return image, torch.LongTensor(input_caption), torch.LongTensor(target_caption), torch.LongTensor([caption_lenght])
+        return image, torch.LongTensor(input_caption), torch.LongTensor([caption_lenght])
 
     def __len__(self):
         return self.dataset_size
+
+# class TestDataset(Dataset):
+
+#     def __init__(
+#         self,
+#         data_folder,
+#         images_folder,
+#         data_type,
+#         max_len,
+#         token_to_id,
+#         transform
+#     ):
+
+#         self.dataset = get_dataset(data_folder)
+
+#         dataset_items = test_dataset.items
+
+#         self.data=iter(test_dataset.items):
+
+#         self.input_captions, self.target_captions, self.captions_lengths = convert_captions_to_Y(
+#             captions_of_tokens, max_len, token_to_id)
+
+#         # logging.info("len dataset of %s is %s",
+#         #              data_type, len(self.images_names))
+#         self.images_folder = images_folder
+#         self.dataset_size = len(self.images_names)
+#         self.transform = transform
+
+#     def __getitem__(self, i):
+
+#         dataset.items
+#         image_name = self.images_folder + self.images_names[i]
+#         image = Image.open(image_name)
+
+#         image = self.transform(image)
+
+#         input_caption = self.input_captions[i]
+#         target_caption = self.target_captions[i]
+#         caption_lenght = self.captions_lengths[i]
+
+#         return image, torch.LongTensor(input_caption), torch.LongTensor(target_caption), torch.LongTensor([caption_lenght])
+
+#     def __len__(self):
+#         print("entrei aqui no LEN", self.dataset_size)
+#         return self.dataset_size
 
 
 class TrialDataset(Dataset):
@@ -55,10 +98,14 @@ class TrialDataset(Dataset):
         data
     ):
         self.data = data
+        print("this is the data", data)
+        self.data_size = len(self.data)
+        print("this is the size of data", self.data_size)
 
     def __getitem__(self, i):
         print("this is my data i", self.data[i])
         return self.data[i]
 
     def __len__(self):
-        return len(self.data)
+        print("what is data size", self.data_size)
+        return self.data_size
