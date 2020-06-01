aebca00c764e8fd6c63c375ffb1e6e3a2aa6e929
RitaRamos <rita.mparada.ramos@gmail.com>
2020-06-01 10:48:07 +0100

    new test

---
+++ src/test2.py
@@ -0,0 +1,92 @@
+import os
+import torch
+import logging
+from args_parser import get_args
+from create_data_files import PATH_RSICD, PATH_DATASETS_RSICD, get_vocab_info, get_dataset
+from models.basic_encoder_decoder_models.encoder_decoder import BasicEncoderDecoderModel
+from models.basic_encoder_decoder_models.encoder_decoder_variants.attention import BasicAttentionModel
+from models.basic_encoder_decoder_models.encoder_decoder_variants.sat import BasicShowAttendAndTellModel
+from models.continuous_encoder_decoder_models.encoder_decoder import ContinuousEncoderDecoderModel
+from models.continuous_encoder_decoder_models.encoder_decoder_variants.attention import ContinuousAttentionModel
+from models.basic_encoder_decoder_models.encoder_decoder_variants.mask import BasicMaskGroundTruthWithPredictionModel
+from models.continuous_encoder_decoder_models.encoder_decoder_variants.attention_relu import ContinuousAttentionReluModel
+from models.continuous_encoder_decoder_models.encoder_decoder_variants.bert import ContinuousBertModel
+from models.continuous_encoder_decoder_models.encoder_decoder_variants.attention_schedule_sampling import ContinuousAttentionWithScheduleSamplingModel
+from models.continuous_encoder_decoder_models.encoder_decoder_variants.attention_schedule_sampling_alt import ContinuousAttentionWithScheduleSamplingAltModel
+from models.continuous_encoder_decoder_models.encoder_decoder_variants.attention_image import ContinuousAttentionImageModel
+from models.continuous_encoder_decoder_models.encoder_decoder_variants.attention_schedule_alt_with_image import ContinuousAttentionImageWithScheduleSamplingModel
+from models.continuous_encoder_decoder_models.encoder_decoder_variants.attention_image_pos import ContinuousAttentionImagePOSModel
+from models.continuous_encoder_decoder_models.encoder_decoder_variants.enc_dec_image import ContinuousEncoderDecoderImageModel
+from models.continuous_encoder_decoder_models.encoder_decoder_variants.enc_dec_image_and_classification import ContinuousEncoderDecoderImageClassificationModel
+from models.continuous_encoder_decoder_models.encoder_decoder_variants.attention_aoa_image import ContinuousAttentionAoAImageModel
+from models.continuous_encoder_decoder_models.encoder_decoder_variants.attention_sat_image import ContinuousSATImageModel
+from models.continuous_encoder_decoder_models.encoder_decoder_variants.attention_aoanet_image import ContinuousAttentionAoANetImageModel
+from models.continuous_encoder_decoder_models.encoder_decoder_variants.attention_product_image import ContinuousProductAttentionImageModel
+from models.continuous_encoder_decoder_models.encoder_decoder_variants.attention_attribute_softmax_image import ContinuousAttentionAttrSoftmaxImageModel
+
+
+from torchvision import transforms
+from PIL import Image
+from preprocess_data.tokens import START_TOKEN, END_TOKEN
+import numpy as np
+import operator
+from nlgeval import NLGEval
+from models.abtract_model import DecodingType
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ['PYTHONHASHSEED'] = '0'
+
+
+if __name__ == "__main__":
+    device = torch.device("cpu")
+
+    args = get_args()
+    print(args.__dict__)
+
+    vocab_info = get_vocab_info(PATH_DATASETS_RSICD+"vocab_info.json")
+    vocab_size, token_to_id, id_to_token, max_len = vocab_info[
+        "vocab_size"], vocab_info["token_to_id"], vocab_info["id_to_token"], vocab_info["max_len"]
+    print("vocab size", vocab_size)
+
+    test_dataset = get_dataset(PATH_DATASETS_RSICD+"test.json")
+
+    model_class = globals()[args.model_class_str]
+    model = model_class(
+        args, vocab_size, token_to_id, id_to_token, max_len, device)
+    model.setup_to_test()
+
+    #scores = model.test(test_dataset)
+
+    # # start test!
+    predicted = {"args": [args.__dict__]}
+    metrics = {}
+
+    print("disable_metrics = True, thus will not compute metrics")
+
+    n_comparations = 0
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],  # mean=IMAGENET_IMAGES_MEAN, std=IMAGENET_IMAGES_STD
+                             std=[0.229, 0.224, 0.225])
+    ])
+
+    # mudar este beam search!
+    decoding_method1 = model.inference_with_greedy
+
+    decoding_method2 = model.inference_with_perplexity
+
+    for img_name, references in test_dataset.items():
+        if img_name != "railwaystation_72.jpg":
+            continue
+
+        image_name = PATH_RSICD + \
+            "raw_dataset/RSICD_images/" + img_name
+        image = Image.open(image_name)
+        image = transform(image)
+        image = image.unsqueeze(0)
+
+        model.decoder.eval()
+        model.encoder.eval()
+
+        text_generated = decoding_method1(image)
+        text_generated = decoding_method2(image)
