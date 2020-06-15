4620b76dfaeb444109501b2644bd381c7d5fd280
RitaRamos <rita.mparada.ramos@gmail.com>
2020-06-15 11:04:13 +0100

    fix

--- src/models/continuous_encoder_decoder_models/encoder_decoder_variants/attention_product_attribute_embedding_image.py
+++ src/models/continuous_encoder_decoder_models/encoder_decoder_variants/attention_product_attribute_embedding_image.py
@@ -16,6 +16,7 @@ import logging
 from torchvision import models
 from preprocess_data.tokens import START_TOKEN, END_TOKEN
 from preprocess_data.images import get_image_extractor, DenseNetFeatureAndAttrExtractor
+import math
 
 
 class FutureAndAttrEncoder(nn.Module):
