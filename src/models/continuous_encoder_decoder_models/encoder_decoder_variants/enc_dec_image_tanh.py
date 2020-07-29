cabbbbe0e85fc71b5a4e61b193f3ebaece0ba1c3
RitaRamos <rita.mparada.ramos@gmail.com>
2020-07-30 00:14:38 +0100

    new model

---
+++ src/models/continuous_encoder_decoder_models/encoder_decoder_variants/enc_dec_image_tanh.py
@@ -0,0 +1,77 @@
+import torchvision
+from torch import nn
+import torch
+from torch.nn.utils.rnn import pack_padded_sequence
+from models.basic_encoder_decoder_models.encoder_decoder import Encoder, Decoder
+from models.abtract_model import AbstractEncoderDecoderModel
+import torch.nn.functional as F
+from embeddings.embeddings import get_embedding_layer
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+from data_preprocessing.preprocess_tokens import OOV_TOKEN
+from embeddings.embeddings import EmbeddingsType
+from models.continuous_encoder_decoder_models.encoder_decoder import ContinuousEncoderDecoderModel
+from embeddings.embeddings import EmbeddingsType
+
+
+class ContinuousDecoderWithImage(Decoder):
+
+    def __init__(self, decoder_dim, embed_dim, embedding_type, vocab_size, token_to_id, post_processing,
+                 encoder_dim=2048, dropout=0.5):
+
+        super(ContinuousDecoderWithImage, self).__init__(decoder_dim, embed_dim,
+                                                         embedding_type, vocab_size, token_to_id, post_processing, encoder_dim, dropout)
+
+        # linear layer to find representation of image
+        self.represent_image = nn.Linear(encoder_dim, embed_dim)
+        self.image_embedding = None
+        self.tanh = nn.Tanh()
+
+        # replace softmax with a embedding layer
+        self.fc = nn.Linear(decoder_dim, embed_dim)
+
+    def init_hidden_state(self, encoder_out):
+        mean_encoder_out = encoder_out.mean(dim=1)
+
+        h = self.init_h(mean_encoder_out)  # (batch_size, decoder_dim)
+        self.image_embedding = self.tanh(self.represent_image(mean_encoder_out))
+
+        return h, h
+
+
+class ContinuousEncoderDecoderImageTanhModel(ContinuousEncoderDecoderModel):
+
+    def __init__(self,
+                 args,
+                 vocab_size,
+                 token_to_id,
+                 id_to_token,
+                 max_len,
+                 device
+                 ):
+        super().__init__(args, vocab_size, token_to_id, id_to_token, max_len, device)
+
+    def _initialize_encoder_and_decoder(self):
+
+        if (self.args.embedding_type not in [embedding.value for embedding in EmbeddingsType]):
+            raise ValueError(
+                "Continuous model should use pretrained embeddings...")
+
+        self.encoder = Encoder(self.args.image_model_type,
+                               enable_fine_tuning=self.args.fine_tune_encoder)
+
+        self.decoder = ContinuousDecoderWithImage(
+            encoder_dim=self.encoder.encoder_dim,
+            decoder_dim=self.args.decoder_dim,
+            embedding_type=self.args.embedding_type,
+            embed_dim=self.args.embed_dim,
+            vocab_size=self.vocab_size,
+            token_to_id=self.token_to_id,
+            post_processing=self.args.post_processing,
+            dropout=self.args.dropout
+        )
+
+        self.decoder.normalize_embeddings(self.args.no_normalization)
+
+        self.encoder = self.encoder.to(self.device)
+        self.decoder = self.decoder.to(self.device)
