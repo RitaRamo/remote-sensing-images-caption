428f09215272039405ca2f53ccc460552dfb49ac
RitaRamos <rita.mparada.ramos@gmail.com>
2020-09-06 14:50:54 +0100

    update with new train and val

--- src/test_scores_coco.py
+++ src/test_scores_coco.py
@@ -3,7 +3,7 @@ from coco_caption.pycocotools.coco import COCO
 from coco_caption.pycocoevalcap.eval import COCOEvalCap
 from args_parser import get_args
 
-from definitions import PATH_RSICD, PATH_DATASETS_RSICD, PATH_EVALUATION_SENTENCES, PATH_EVALUATION_SCORES
+from definitions import PATH_RSICD, PATH_DATASETS_RSICD_NEW_TRAIN_AND_VAL, PATH_EVALUATION_SENTENCES, PATH_EVALUATION_SCORES
 from utils.enums import EvalDatasetType
 
 
@@ -14,16 +14,16 @@ if __name__ == "__main__":
 
     # Choose dataset to evaluate the model:
     if args.eval_dataset_type == EvalDatasetType.VAL.value:
-        test_path = PATH_DATASETS_RSICD + "val_coco_format.json"
+        test_path = PATH_DATASETS_RSICD_NEW_TRAIN_AND_VAL + "val_coco_format.json"
         decoding_args = args.file_name + "_v_" + args.decodying_type + "_" + str(args.n_beam) + '_coco'
 
     elif args.eval_dataset_type == EvalDatasetType.TRAIN_AND_VAL.value:
-        test_path = PATH_DATASETS_RSICD + "train_and_val_coco_format.json"
+        test_path = PATH_DATASETS_RSICD_NEW_TRAIN_AND_VAL + "train_and_val_coco_format.json"
         decoding_args = args.file_name + "_tv_" + args.decodying_type + "_" + str(args.n_beam) + '_coco'
 
     else:  # test set
         decoding_args = args.file_name + "_" + args.decodying_type + "_" + str(args.n_beam) + '_coco'
-        test_path = PATH_DATASETS_RSICD + "test_coco_format.json"
+        test_path = PATH_DATASETS_RSICD_NEW_TRAIN_AND_VAL + "test_coco_format.json"
 
     generated_sentences_path = PATH_EVALUATION_SENTENCES + decoding_args
 
