7ac5d4b10fe6c9cb080521c89e1ad326cacb9792
RitaRamos <rita.mparada.ramos@gmail.com>
2020-09-16 16:46:39 +0100

    new conf

--- src/models/continuous_encoder_decoder_models/encoder_decoder_variants/enc_dec_image_emb.py
+++ src/models/continuous_encoder_decoder_models/encoder_decoder_variants/enc_dec_image_emb.py
@@ -2,7 +2,7 @@ import torchvision
 from torch import nn
 import torch
 from torch.nn.utils.rnn import pack_padded_sequence
-from models.basic_encoder_decoder_models.encoder_decoder import Encoder, Decoder
+from models.basic_encoder_decoder_models.encoder_decoder import Decoder
 from models.abtract_model import AbstractEncoderDecoderModel
 import torch.nn.functional as F
 from embeddings.embeddings import get_embedding_layer
@@ -12,6 +12,62 @@ from data_preprocessing.preprocess_tokens import OOV_TOKEN
 from embeddings.embeddings import EmbeddingsType
 from models.continuous_encoder_decoder_models.encoder_decoder import ContinuousEncoderDecoderModel
 from embeddings.embeddings import EmbeddingsType
+from data_preprocessing.preprocess_images import get_image_model
+
+
+class Encoder(nn.Module):
+    """
+    Encoder.
+    """
+
+    def __init__(self, model_type, encoded_image_size=14, enable_fine_tuning=False):
+        super(Encoder, self).__init__()
+        self.enc_image_size = encoded_image_size
+
+        self.model, self.encoder_dim = get_image_model(model_type)
+
+        # Resize image to fixed size to allow input images of variable size
+        self.adaptive_pool = nn.AdaptiveAvgPool2d(
+            (encoded_image_size, encoded_image_size))
+
+        # Disable calculation of all gradients
+        for p in self.model.parameters():
+            p.requires_grad = False
+
+        # Enable calculation of some gradients for fine tuning
+        self.fine_tune(enable_fine_tuning)
+
+    def forward(self, images):
+        """
+        Forward propagation.
+        :param images: images, a tensor of dimensions (batch_size, 3, image_size, image_size)
+        :return: encoded images
+        """
+        out = self.model(
+            images)  # (batch_size, 2048, image_size/32, image_size/32)
+
+        #out = self.model.extract_features(images)
+        # #print("image size", out.size())
+
+        # # (batch_size, 2048, encoded_image_size, encoded_image_size)
+        out = self.adaptive_pool(out)
+        # # (batch_size, encoded_image_size, encoded_image_size, 2048)
+        # # (later on the intermidiate dims are flatten: (prepare_inputs)
+        # # (batch_size, encoded_image_size*encoded_image_size, 2048)
+        out = out.permute(0, 2, 3, 1)
+
+        return out
+
+    def fine_tune(self, enable_fine_tuning):
+        """
+        Allow or prevent the computation of gradients for convolutional blocks 2 through 4 of the encoder.
+        :param fine_tune: Allow?
+        """
+        # If fine-tuning, only fine-tune convolutional blocks 2 through 4
+        for c in list(self.model.children())[-1]:  # [5:]:#toda!!!
+            print("Lets fine tune this childreen:", c)
+            for p in c.parameters():
+                p.requires_grad = enable_fine_tuning
 
 
 class ContinuousDecoderWithImage(Decoder):
