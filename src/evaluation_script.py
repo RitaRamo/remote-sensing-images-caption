a4563c2f47cb7fd106feb08c50061c246c192f14
RitaRamos <rita.mparada.ramos@gmail.com>
2020-09-21 11:20:35 +0100

    new conf

--- src/evaluation_script.py
+++ src/evaluation_script.py
@@ -5,23 +5,16 @@ import subprocess
 import sys
 import time
 
-list_files = ['split_fine_encdec_1comp_eff_noaug_notnormalized.txt',
-              'split_fine_encdec_2comp_eff_noaug_notnormalized.txt',
-              'split_fine_encdec_13comp_eff_noaug_notnormalized.txt',
-              'split_fine_encdec_2comphdsim_eff_noaug_notnormalized.txt',
-              'split_fine_encdec_2comphdsimd1_eff_noaug_notnormalized.txt',
-              'split_fine_encdec_4comphdall_eff_noaug_notnormalized.txt',
-              'split_fine_encdec_2comphd_eff_noaug_notnormalized.txt',
-              'split_fine_encdec_2comphdsimf1_eff_noaug_notnormalized.txt',
-              'split_fine_encdec_4comphd_eff_noaug_notnormalized.txt',
-              'split_fine_encdec_2comphddiv2_eff_noaug_notnormalized.txt']
+list_files = ['
+              'ucml_fine_encdec_3comp_eff_noaug_notnormalized.txt',
+              'xasa.txt']
 
 for eval_file in list_files:
-    subprocess.Popen(["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file,
-                      "--decodying_type=beam", "--n_beam=10", "--test_set"])
-    # subprocess.Popen(["python2", "src/test_scores_coco.py", "@experiments/conf_files/" + eval_file,
-    #                   "--decodying_type=beam", "--n_beam=10", "--test_set"])
-    # subprocess.Popen(["python3", "src/test_scores_bertscore.py", "@experiments/conf_files/" + eval_file,
-    #                   "--decodying_type=beam", "--n_beam=10", "--test_set"])
+    subprocess.run(["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file,
+                    "--decodying_type=beam", "--n_beam=10"])
+    subprocess.run(["python2", "src/test_scores_coco.py", "@experiments/conf_files/" + eval_file,
+                    "--decodying_type=beam", "--n_beam=10"])
+    subprocess.run(["python3", "src/test_scores_bertscore.py", "@experiments/conf_files/" + eval_file,
+                    "--decodying_type=beam", "--n_beam=10"])
 
 sys.exit()
