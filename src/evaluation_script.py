925fef6e1f47d117c87167073a943b56d63af294
RitaRamos <rita.mparada.ramos@gmail.com>
2020-09-21 11:45:15 +0100

    update script

--- src/evaluation_script.py
+++ src/evaluation_script.py
@@ -1,19 +1,21 @@
 import os
-import psutil
-import re
-import subprocess
 import sys
-import time
 
-list_files = ['ucml_fine_encdec_3comp_eff_noaug_notnormalized.txt',
-              'xasa.txt']
+eval_file = sys.argv[0]
+print("File that will be evaluated", eval_file)
 
-for eval_file in list_files:
-    subprocess.run(["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file,
-                    "--decodying_type=beam", "--n_beam=10"])
-    subprocess.run(["python2", "src/test_scores_coco.py", "@experiments/conf_files/" + eval_file,
-                    "--decodying_type=beam", "--n_beam=10"])
-    subprocess.run(["python3", "src/test_scores_bertscore.py", "@experiments/conf_files/" + eval_file,
-                    "--decodying_type=beam", "--n_beam=10"])
+subprocess.run(["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file,
+                "--decodying_type=beam", "--n_beam=10"])
+subprocess.run(["python2", "src/test_scores_coco.py", "@experiments/conf_files/" + eval_file,
+                "--decodying_type=beam", "--n_beam=10"])
+subprocess.run(["python3", "src/test_scores_bertscore.py", "@experiments/conf_files/" + eval_file,
+                "--decodying_type=beam", "--n_beam=10"])
+
+subprocess.run(["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file,
+                "--decodying_type=beam", "--n_beam=10", "--eval_dataset_type=val"])
+subprocess.run(["python2", "src/test_scores_coco.py", "@experiments/conf_files/" + eval_file,
+                "--decodying_type=beam", "--n_beam=10", "--eval_dataset_type=val"])
+subprocess.run(["python3", "src/test_scores_bertscore.py", "@experiments/conf_files/" + eval_file,
+                "--decodying_type=beam", "--n_beam=10", "--eval_dataset_type=val"])
 
 sys.exit()
