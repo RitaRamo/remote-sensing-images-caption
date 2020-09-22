510362fa0b24500f4e5828e0403e507af55c626c
RitaRamos <rita.mparada.ramos@gmail.com>
2020-09-22 10:06:22 +0100

    new model

--- src/models/basic_encoder_decoder_models/encoder_decoder.py
+++ src/models/basic_encoder_decoder_models/encoder_decoder.py
@@ -69,7 +69,7 @@ class Encoder(nn.Module):
         if enable_fine_tuning:
             print("fine-tuning encoder")
             if self.model_type == ImageNetModelsPretrained.MULTILABEL_ALL_EFFICIENCENET.value:
-                for c in list(self.model.children())[-6:-4]:  # [-6:-1]
+                for c in list(self.model.children())[-6:-5]:  # [-6:-1]
                     print("unfreezing eff, layer", c)
                     for p in c.parameters():
                         p.requires_grad = enable_fine_tuning
