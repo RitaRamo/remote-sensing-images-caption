d452d601c4f6a04911516bc4eb48bf649a10567c
RitaRamos <rita.mparada.ramos@gmail.com>
2020-09-28 23:05:07 +0100

    new model

--- src/evaluation_script.py
+++ src/evaluation_script.py
@@ -2,28 +2,46 @@ import sys
 import subprocess
 
 
-eval_file = sys.argv[1]
+#eval_file = sys.argv[1]
 
-print("File that will be evaluated", eval_file)
+#print("File that will be evaluated", eval_file)
 
-for n_beam in [1, 3, 5, 15]:
-    print("n_beam", n_beam)
+for eval_file in ["
+                  "flick_fine_encdec_discrete_effimgnt_noaug_notnormalized.txt",
+                  "flick_fine_encdec_discrete_effimgnt_noaug_notnormalized.txt",
+                  "flickr_fine_encdec_1comp_effflickr_noaug_notnormalized.txt",
+                  "flickr_fine_encdec_4comp_effuflickr_noaug_notnormalized.txt"]:
 
-    n_beam = str(n_beam)
+    print("File that will be evaluated", eval_file)
 
-    subprocess.run(["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file,
-                    "--decodying_type=beam", "--n_beam=" + n_beam])
-    subprocess.run(["python2", "src/test_scores_coco.py", "@experiments/conf_files/" + eval_file,
-                    "--decodying_type=beam", "--n_beam=" + n_beam])
-    subprocess.run(["python3", "src/test_scores_bertscore.py", "@experiments/conf_files/" + eval_file,
-                    "--decodying_type=beam", "--n_beam=" + n_beam])
+    for n_beam in [1, 3, 5, 15]:
+        print("n_beam", n_beam)
 
-    subprocess.run(["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file,
-                    "--decodying_type=beam", "--n_beam=" + n_beam, "--eval_dataset_type=val"])
-    subprocess.run(["python2", "src/test_scores_coco.py", "@experiments/conf_files/" + eval_file,
-                    "--decodying_type=beam", "--n_beam=" + n_beam, "--eval_dataset_type=val"])
-    subprocess.run(["python3", "src/test_scores_bertscore.py", "@experiments/conf_files/" + eval_file,
-                    "--decodying_type=beam", "--n_beam=" + n_beam, "--eval_dataset_type=val"])
+        n_beam = str(n_beam)
+
+        subprocess.run(["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file,
+                        "--decodying_type=beam", "--n_beam=" + n_beam])
+        subprocess.run(["python2", "src/test_scores_coco.py", "@experiments/conf_files/" + eval_file,
+                        "--decodying_type=beam", "--n_beam=" + n_beam])
+        subprocess.run(["python3", "src/test_scores_bertscore.py", "@experiments/conf_files/" + eval_file,
+                        "--decodying_type=beam", "--n_beam=" + n_beam])
+
+        subprocess.run(["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file,
+                        "--decodying_type=beam", "--n_beam=" + n_beam, "--eval_dataset_type=val"])
+        subprocess.run(["python2", "src/test_scores_coco.py", "@experiments/conf_files/" + eval_file,
+                        "--decodying_type=beam", "--n_beam=" + n_beam, "--eval_dataset_type=val"])
+        subprocess.run(["python3", "src/test_scores_bertscore.py", "@experiments/conf_files/" + eval_file,
+                        "--decodying_type=beam", "--n_beam=" + n_beam, "--eval_dataset_type=val"])
+
+    n_beam = 10
+    for min_len in [2, 3, 5, 7, 10]:
+        subprocess.run(["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file,
+                        "--decodying_type=beam", "--n_beam=" + n_beam, "--min_len=" + min_len, "--eval_dataset_type=val"])
+        subprocess.run(["python2", "src/test_scores_coco.py", "@experiments/conf_files/" + eval_file,
+                        "--decodying_type=beam", "--n_beam=" + n_beam, "--min_len=" + min_len, "--eval_dataset_type=val"])
+        subprocess.run(["python3", "src/test_scores_bertscore.py", "@experiments/conf_files/" + eval_file,
+                        "--decodying_type=beam", "--n_beam=" + n_beam, "--min_len=" + min_len, "--eval_dataset_type=val"])
+
 
 # greedy
 # subprocess.run(["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file])
--- src/test_generate_sentences.py
+++ src/test_generate_sentences.py
@@ -149,7 +149,7 @@ if __name__ == "__main__":
         # model.decoder.eval()
         # model.encoder.eval()
 
-        text_generated = decoding_method(image, args.n_beam)
+        text_generated = decoding_method(image, args.n_beam, args.min_len)
 
         list_hipotheses.append({
             "image_id": img_id,
