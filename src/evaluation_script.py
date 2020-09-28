c9b60400ff4bfccff05b1be7aa0e1b532e32c2e7
RitaRamos <rita.mparada.ramos@gmail.com>
2020-09-28 16:52:59 +0100

    new eval

--- src/evaluation_script.py
+++ src/evaluation_script.py
@@ -26,12 +26,12 @@ for n_beam in [1, 5, 15]:
                     "--decodying_type=beam", "--n_beam=" + n_beam, "--eval_dataset_type=val"])
 
 # greedy
-subprocess.run(["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file])
-subprocess.run(["python2", "src/test_scores_coco.py", "@experiments/conf_files/" + eval_file])
-subprocess.run(["python3", "src/test_scores_bertscore.py", "@experiments/conf_files/" + eval_file])
+# subprocess.run(["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file])
+# subprocess.run(["python2", "src/test_scores_coco.py", "@experiments/conf_files/" + eval_file])
+# subprocess.run(["python3", "src/test_scores_bertscore.py", "@experiments/conf_files/" + eval_file])
 
-subprocess.run(["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file])
-subprocess.run(["python2", "src/test_scores_coco.py", "@experiments/conf_files/" + eval_file])
-subprocess.run(["python3", "src/test_scores_bertscore.py", "@experiments/conf_files/" + eval_file])
+# subprocess.run(["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file])
+# subprocess.run(["python2", "src/test_scores_coco.py", "@experiments/conf_files/" + eval_file])
+# subprocess.run(["python3", "src/test_scores_bertscore.py", "@experiments/conf_files/" + eval_file])
 
 sys.exit()
