a748c782cfd90726099c28863ffffb0051d9c9dc
RitaRamos <rita.mparada.ramos@gmail.com>
2020-09-28 12:11:00 +0100

    improve n beam

--- src/evaluation_script.py
+++ src/evaluation_script.py
@@ -9,6 +9,8 @@ print("File that will be evaluated", eval_file)
 for n_beam in [1, 5, 15]:
     print("n_beam", n_beam)
 
+    n_beam = str(n_beam)
+
     subprocess.run(["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file,
                     "--decodying_type=beam", "--n_beam=" + n_beam])
     subprocess.run(["python2", "src/test_scores_coco.py", "@experiments/conf_files/" + eval_file,
