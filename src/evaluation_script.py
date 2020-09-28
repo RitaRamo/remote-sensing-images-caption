5d6c084ecda59e80ae4819fb2c527ff93496c8fb
RitaRamos <rita.mparada.ramos@gmail.com>
2020-09-28 20:33:06 +0100

    new model

--- src/evaluation_script.py
+++ src/evaluation_script.py
@@ -6,7 +6,7 @@ eval_file = sys.argv[1]
 
 print("File that will be evaluated", eval_file)
 
-for n_beam in [1, 5, 15]:
+for n_beam in [1, 3, 5, 15]:
     print("n_beam", n_beam)
 
     n_beam = str(n_beam)
@@ -14,9 +14,9 @@ for n_beam in [1, 5, 15]:
     subprocess.run(["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file,
                     "--decodying_type=beam", "--n_beam=" + n_beam])
     subprocess.run(["python2", "src/test_scores_coco.py", "@experiments/conf_files/" + eval_file,
-                    "--decodying_type=beam", "--n_beam=10" + n_beam])
+                    "--decodying_type=beam", "--n_beam=" + n_beam])
     subprocess.run(["python3", "src/test_scores_bertscore.py", "@experiments/conf_files/" + eval_file,
-                    "--decodying_type=beam", "--n_beam=10" + n_beam])
+                    "--decodying_type=beam", "--n_beam=" + n_beam])
 
     subprocess.run(["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file,
                     "--decodying_type=beam", "--n_beam=" + n_beam, "--eval_dataset_type=val"])
