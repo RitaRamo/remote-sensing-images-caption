cc0a0933cd0ad41aa7156fef49fc5a908a68bfea
RitaRamos <rita.mparada.ramos@gmail.com>
2020-09-29 10:44:20 +0100

    update conf

--- src/evaluation_script.py
+++ src/evaluation_script.py
@@ -7,8 +7,8 @@ import subprocess
 #print("File that will be evaluated", eval_file)
 
 for eval_file in [
-    # "flick_fine_encdec_discrete_effimgnt_noaug_notnormalized.txt",
-    # "flick_fine_encdec_discrete_effimgnt_noaug_notnormalized.txt",
+    "flick_fine_encdec_discrete_effimgnt_noaug_notnormalized.txt",
+    "flick_fine_encdec_discrete_effflick_noaug_notnormalized.txt",
     "flickr_fine_encdec_1comp_effflickr_noaug_notnormalized.txt",
     "flickr_fine_encdec_4comp_effuflickr_noaug_notnormalized.txt"
 ]:
@@ -44,7 +44,6 @@ for eval_file in [
         subprocess.run(["python3", "src/test_scores_bertscore.py", "@experiments/conf_files/" + eval_file,
                         "--decodying_type=beam", "--n_beam=" + n_beam, "--min_len=" + min_len, "--eval_dataset_type=val"])
 
-        print(stop)
 # greedy
 # subprocess.run(["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file])
 # subprocess.run(["python2", "src/test_scores_coco.py", "@experiments/conf_files/" + eval_file])
