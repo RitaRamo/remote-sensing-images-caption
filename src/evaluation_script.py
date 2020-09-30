5786e724f64a99d1d430516768f163f0a309a03d
RitaRamos <rita.mparada.ramos@gmail.com>
2020-09-30 01:01:55 +0100

    update

--- src/evaluation_script.py
+++ src/evaluation_script.py
@@ -39,10 +39,10 @@ for eval_file in [
         min_len = str(min_len)
         subprocess.run(["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file,
                         "--decodying_type=beam", "--n_beam=" + n_beam, "--min_len=" + min_len, "--eval_dataset_type=val"])
-        subprocess.run(["python2", "src/test_scores_coco.py", "@experiments/conf_files/" + eval_file,
-                        "--decodying_type=beam", "--n_beam=" + n_beam, "--min_len=" + min_len, "--eval_dataset_type=val"])
-        subprocess.run(["python3", "src/test_scores_bertscore.py", "@experiments/conf_files/" + eval_file,
-                        "--decodying_type=beam", "--n_beam=" + n_beam, "--min_len=" + min_len, "--eval_dataset_type=val"])
+        # subprocess.run(["python2", "src/test_scores_coco.py", "@experiments/conf_files/" + eval_file,
+        # "--decodying_type=beam", "--n_beam=" + n_beam, "--min_len=" + min_len, "--eval_dataset_type=val"])
+        # subprocess.run(["python3", "src/test_scores_bertscore.py", "@experiments/conf_files/" + eval_file,
+        # "--decodying_type=beam", "--n_beam=" + n_beam, "--min_len=" + min_len, "--eval_dataset_type=val"])
         print(stop)
 # greedy
 # subprocess.run(["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file])
