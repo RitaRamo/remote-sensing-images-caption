1e847552c3cdbf6558ffc65349ff32a3099431cd
RitaRamos <rita.mparada.ramos@gmail.com>
2020-09-30 01:05:49 +0100

    update

--- src/evaluation_script.py
+++ src/evaluation_script.py
@@ -38,7 +38,7 @@ for eval_file in [
     for min_len in [2, 3, 5, 7, 10]:
         min_len = str(min_len)
         subprocess.run(
-            ["PYTHONHASHSEED=0", "python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file,
+            ["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file,
              "--decodying_type=beam", "--n_beam=" + n_beam, "--min_len=" + min_len, "--eval_dataset_type=val"])
         # subprocess.run(["python2", "src/test_scores_coco.py", "@experiments/conf_files/" + eval_file,
         # "--decodying_type=beam", "--n_beam=" + n_beam, "--min_len=" + min_len, "--eval_dataset_type=val"])
