8a0e6a4027cab97c682b84d7904ee38d4d784806
RitaRamos <rita.mparada.ramos@gmail.com>
2020-10-08 20:13:37 +0100

    add new beams

--- src/evaluation_script.py
+++ src/evaluation_script.py
@@ -24,21 +24,19 @@ for eval_file in [
     subprocess.run(["python3", "src/test_scores_bertscore.py",
                     "@experiments/conf_files/" + eval_file, "--eval_dataset_type=val"])
 
-    subprocess.run(["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file,
-                    "--decodying_type=beam", "--n_beam=" + n_beam, "--min_len=" + 0, "--rep_window=" + 0,
-                    "--eval_dataset_type=val"])
+    for beam_type in ["beam_wt_refinement", "beam_comp", "beam_tutorial", "beam"]:
 
-    subprocess.run(["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file,
-                    "--decodying_type=beam_comp", "--n_beam=" + n_beam, "--min_len=" + 0, "--rep_window=" + 0,
-                    "--eval_dataset_type=val"])
+        subprocess.run(["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file,
+                        "--decodying_type=" + beam_type, "--n_beam=10", "--min_len=0", "--rep_window=0",
+                        "--eval_dataset_type=val"])
 
-    subprocess.run(["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file,
-                    "--decodying_type=beam_tutorial", "--n_beam=" + n_beam, "--min_len=" + 0, "--rep_window=" + 0,
-                    "--eval_dataset_type=val"])
+        subprocess.run(["python2", "src/test_scores_coco.py", "@experiments/conf_files/" + eval_file,
+                        "--decodying_type=" + beam_type, "--n_beam=10", "--min_len=0", "--rep_window=0",
+                        "--eval_dataset_type=val"])
 
-    subprocess.run(["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file,
-                    "--decodying_type=beam_wt_refinement", "--n_beam=" + n_beam, "--min_len=" + 0, "--rep_window=" + 0,
-                    "--eval_dataset_type=val"])
+        subprocess.run(["python3", "src/test_scores_bertscore.py", "@experiments/conf_files/" + eval_file,
+                        "--decodying_type=" + beam_type, "--n_beam=10", "--min_len=0", "--rep_window=0",
+                        "--eval_dataset_type=val"])
 
     # for n_beam in [1, 2, 3, 5, 10]:
     #     print("n_beam", n_beam)
