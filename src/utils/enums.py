360fc4597fe37feb44e192f27a3de5769e3bb293
RitaRamos <rita.mparada.ramos@gmail.com>
2020-11-30 11:14:37 +0000

    new conf img model

--- src/utils/enums.py
+++ src/utils/enums.py
@@ -25,6 +25,7 @@ class ImageNetModelsPretrained(Enum):
     EFFICIENCENET_RSICD_CAPTION_EMBEDDINGS = "efficient_net_rsicd_caption_emb"
     EFFICIENCENET_RSICD_CAPTION_GLOVE_EMBEDDINGS = "efficient_net_rsicd_caption_emb_glove"
     EFFICIENCENET_RSICD_CAPTION_GLOVE_EMBEDDINGS_SMOOTHL1 = "efficient_net_rsicd_caption_emb_glove_smoothl1"
+    EFFICIENCENET_RSICD_CAPTION_FASTTEXT_EMBEDDINGS_SMOOTHL1 = "efficient_net_rsicd_caption_emb_fasttext_smoothl1"
     EFFICIENCENET_EMBEDDINGS = "efficient_net_embeddings"
     EFFICIENCENET_UCM = "efficient_net_ucm"
     EFFICIENCENET_FLICKR8K = "efficient_net_flickr8k"
