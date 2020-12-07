42a8019c59ef64158d4ecffdc6fc8adfd79caf11
RitaRamos <rita.mparada.ramos@gmail.com>
2020-12-07 11:30:57 +0000

    update

--- src/models/continuous_encoder_decoder_models/encoder_decoder_variants/attention_scale_product_h.py
+++ src/models/continuous_encoder_decoder_models/encoder_decoder_variants/attention_scale_product_h.py
@@ -12,6 +12,7 @@ from data_preprocessing.preprocess_tokens import OOV_TOKEN, START_TOKEN, END_TOK
 from embeddings.embeddings import EmbeddingsType
 from models.continuous_encoder_decoder_models.encoder_decoder import ContinuousEncoderDecoderModel
 from embeddings.embeddings import EmbeddingsType
+from data_preprocessing.preprocess_images import get_image_model
 
 class Encoder(nn.Module):
     """
