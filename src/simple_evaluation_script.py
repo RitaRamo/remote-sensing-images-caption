5bbeb3e580e092c30781b7eaed6cca51e3c58b7b
RitaRamos <rita.mparada.ramos@gmail.com>
2020-12-15 23:53:51 +0000

    beam cont

--- src/simple_evaluation_script.py
+++ src/simple_evaluation_script.py
@@ -5,11 +5,25 @@ eval_file = sys.argv[1]
 
 print("File that will be evaluated", eval_file)
 
-subprocess.run(["python3", "src/test_generate_sentences.py",
-                "@experiments/conf_files/" + eval_file, "--eval_dataset_type=val"])
-subprocess.run(["python2", "src/test_scores_coco.py",
-                "@experiments/conf_files/" + eval_file, "--eval_dataset_type=val"])
-subprocess.run(["python3", "src/test_scores_bertscore.py",
-                "@experiments/conf_files/" + eval_file, "--eval_dataset_type=val"])
+subprocess.run(["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file,
+                        "--decodying_type=beam_wt_refinement", "--n_beam=3", "--min_len=0", "--rep_window=0"])
+
+subprocess.run(["python2", "src/test_scores_coco.py", "@experiments/conf_files/" + eval_file,
+                "--decodying_type=beam_wt_refinement", "--n_beam=3", "--min_len=0", "--rep_window=0"])
+
+subprocess.run(["python3", "src/test_scores_bertscore.py", "@experiments/conf_files/" + eval_file,
+                "--decodying_type=beam_wt_refinement", "--n_beam=3", "--min_len=0", "--rep_window=0"])
+
+subprocess.run(["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file,
+                        "--decodying_type=beam_wt_refinement", "--n_beam=3", "--min_len=0", "--rep_window=0",
+                        "--eval_dataset_type=val"])
+
+subprocess.run(["python2", "src/test_scores_coco.py", "@experiments/conf_files/" + eval_file,
+                "--decodying_type=beam_wt_refinement", "--n_beam=3", "--min_len=0", "--rep_window=0",
+                "--eval_dataset_type=val"])
+
+subprocess.run(["python3", "src/test_scores_bertscore.py", "@experiments/conf_files/" + eval_file,
+                "--decodying_type=beam_wt_refinement", "--n_beam=3", "--min_len=0", "--rep_window=0",
+                "--eval_dataset_type=val"])
 
 sys.exit()
