4635e8958e2c4b36a07b24bc3caa0e02a8a4a309
RitaRamos <rita.mparada.ramos@gmail.com>
2021-02-03 10:25:15 +0000

    trying to fix meteor bug float

--- src/coco_caption/pycocoevalcap/meteor/meteor.py
+++ src/coco_caption/pycocoevalcap/meteor/meteor.py
@@ -55,8 +55,8 @@ class Meteor:
         # SCORE ||| reference 1 words ||| reference n words ||| hypothesis words
         hypothesis_str = hypothesis_str.replace('|||', '').replace('  ', ' ')
         score_line = ' ||| '.join(('SCORE', ' ||| '.join(reference_list), hypothesis_str))
-        #self.meteor_p.stdin.write('{}\n'.format(score_line))
-        self.meteor_p.stdin.write('{}\n'.format(eval_line).replace('.',','))
+        self.meteor_p.stdin.write('{}\n'.format(score_line))
+        #self.meteor_p.stdin.write('{}\n'.format(eval_line).replace('.',','))
         return self.meteor_p.stdout.readline().strip()
 
     def _score(self, hypothesis_str, reference_list):
