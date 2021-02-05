387af44f5562573cc0fe576ec5d23a0f3f35cf66
RitaRamos <rita.mparada.ramos@gmail.com>
2021-02-05 23:24:49 +0000

    back as before

--- src/data_preprocessing/preprocess_images.py
+++ src/data_preprocessing/preprocess_images.py
@@ -181,7 +181,7 @@ def get_image_model(model_type):
         image_model.load_state_dict(checkpoint['model'])
 
         return image_model, encoder_dim
-        
+
     elif model_type == ImageNetModelsPretrained.EFFICIENCENETB5_RSICD_ALLCAPTIONS_GLOVE_EMBEDDINGS_SMOOTHL1.value:
         # https://github.com/lukemelas/EfficientNet-PyTorch/pull/194
         logging.info("image model with efficientnet B5 ALL CAPS RSICD multi-label classification with emb caption glove smoorhl1")
@@ -221,6 +221,24 @@ def get_image_model(model_type):
         logging.info("image model with efficientnet B7 RSICD multi-label classification with emb caption glove smoorhl1")
 
         checkpoint = torch.load('experiments/results/classification_efficientnet_b7_rsicd_embedding_caption_glove_smoothl1.pth.tar')
+        emb_dim = 300
+
+        image_model = EfficientNet.from_pretrained('efficientnet-b7')
+        encoder_dim = image_model._fc.in_features
+        image_model._fc = nn.Linear(encoder_dim, emb_dim)
+
+        print("encoder dim", encoder_dim)
+
+        image_model.load_state_dict(checkpoint['model'])
+
+        return image_model, encoder_dim
+
+        
+    elif model_type == ImageNetModelsPretrained.EFFICIENCENETB7_OONE_RSICD_CAPTION_GLOVE_EMBEDDINGS_SMOOTHL1.value:
+        # https://github.com/lukemelas/EfficientNet-PyTorch/pull/194
+        logging.info("image model with efficientnet B7 RSICD multi-label classification with emb caption glove smoorhl1")
+
+        checkpoint = torch.load('experiments/results/classification_efficientnet_b7old_onersicd_embedding_caption_glove_smoothl1.pth.tar')
         emb_dim = 300
 
         image_model = EfficientNet.from_pretrained('efficientnet-b7')
