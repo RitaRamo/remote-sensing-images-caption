dc343133ff3f2aaa1014fa6c0dbeb782e61a73d5
RitaRamos <rita.mparada.ramos@gmail.com>
2021-02-11 16:29:41 +0000

    fix get info for number of epochs

--- src/scripts_multilabel_classification/multi_embedding.py/load_info.py
+++ src/scripts_multilabel_classification/multi_embedding.py/load_info.py
@@ -18,15 +18,15 @@ def _load_weights_from_checkpoint(self, checkpoint_path):
 if __name__ == "__main__":
 
     print("\nPAPER300ef5allNEWS_newsplit_fine_attenscalepro (efficientb5_net_rsicd_allcaptions_emb_glove_smoothl1)")
-    path="experiments/results/classification_efficientnet_b5all_rsicd_embedding_caption_glove_smoothl1.pth.tar":
+    path="experiments/results/classification_efficientnet_b5all_rsicd_embedding_caption_glove_smoothl1.pth.tar"
     _load_weights_from_checkpoint(path)
 
     print("\nPAPER300ef5oneNEWS_newsplit_fine_attenscalepro")
-    path="experiments/results/classification_efficientnet_b5one_rsicd_embedding_caption_glove_smoothl1.pth.tar":
+    path="experiments/results/classification_efficientnet_b5one_rsicd_embedding_caption_glove_smoothl1.pth.tar"
     _load_weights_from_checkpoint(path)
 
     print("\PAPER300ef7oneOLD_fine_attenscaleprod_3comp_ef  (efficientb7oone_net_rsicd_caption_emb_glove_smoothl1)")
-    path="experiments/results/classification_efficientnet_b7old_onersicd_embedding_caption_glove_smoothl1.pth.tar":
+    path="experiments/results/classification_efficientnet_b7old_onersicd_embedding_caption_glove_smoothl1.pth.tar"
     _load_weights_from_checkpoint(path)
 
     print("\npaperr_newsplit_fine_attenscaleprod_1compr_eff")
