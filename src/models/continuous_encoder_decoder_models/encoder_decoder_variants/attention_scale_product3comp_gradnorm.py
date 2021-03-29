3aa41b788446864835b35d68cc00bf51b4ccc374
RitaRamos <rita.mparada.ramos@gmail.com>
2021-03-29 12:05:47 +0100

    new mmr

--- src/models/continuous_encoder_decoder_models/encoder_decoder_variants/attention_scale_product3comp_gradnorm.py
+++ src/models/continuous_encoder_decoder_models/encoder_decoder_variants/attention_scale_product3comp_gradnorm.py
@@ -701,7 +701,7 @@ class ContinuousScaleProductAttention3CompGradNormModel(ContinuousEncoderDecoder
             return generated_sentence  # input_caption
 
     def inference_with_greedy_smoothl1_mmr(self, image, n_solutions=0, min_len=0, repetition_window=0, max_len=50):
-        alpha=0.95
+        alpha=0.99
         with torch.no_grad():  # no need to track history
 
             decoder_sentence = []
