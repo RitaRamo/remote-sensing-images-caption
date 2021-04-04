a8775e9274e34bc3006aba83f6ac62b26a42c6c6
RitaRamos <rita.mparada.ramos@gmail.com>
2021-04-04 23:53:50 +0100

    new conf

--- src/evaluation_script.py
+++ src/evaluation_script.py
@@ -13,11 +13,34 @@ for eval_file in [
 ]:
 
     subprocess.run(["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file,
-                        "--decodying_type=greedy_smoothl1_mmr", "--rep_window=2", "--min_len=2","--max_len=1005"])
+                        "--decodying_type=beam_wt_refinement", "--n_beam=3"])
 
     subprocess.run(["python2", "src/test_scores_coco.py", "@experiments/conf_files/" + eval_file,
-                    "--decodying_type=greedy_smoothl1_mmr", "--rep_window=2", "--min_len=2", "--max_len=1005"])
+                    "--decodying_type=beam_wt_refinement", "--n_beam=3"])
 
+    subprocess.run(["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file,
+                        "--decodying_type=beam", "--n_beam=3"])
+
+    subprocess.run(["python2", "src/test_scores_coco.py", "@experiments/conf_files/" + eval_file,
+                    "--decodying_type=beam", "--n_beam=3"])
+
+    subprocess.run(["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file,
+                        "--decodying_type=beam", "--n_beam=2"])
+
+    subprocess.run(["python2", "src/test_scores_coco.py", "@experiments/conf_files/" + eval_file,
+                    "--decodying_type=beam", "--n_beam=2"])
+
+    subprocess.run(["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file,
+                        "--decodying_type=beam", "--n_beam=4"])
+
+    subprocess.run(["python2", "src/test_scores_coco.py", "@experiments/conf_files/" + eval_file,
+                    "--decodying_type=beam", "--n_beam=4"])
+
+    subprocess.run(["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file,
+                        "--decodying_type=beam", "--n_beam=5"])
+
+    subprocess.run(["python2", "src/test_scores_coco.py", "@experiments/conf_files/" + eval_file,
+                    "--decodying_type=beam", "--n_beam=5"])
 
     # subprocess.run(["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file,
     #                     "--decodying_type=greedy_smoothl1_mmr", "--min_len=3", "--rep_window=3"])
