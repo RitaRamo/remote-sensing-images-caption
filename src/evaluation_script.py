8fb8989dafa8d3eed9f646ec37a31c1625df3986
RitaRamos <rita.mparada.ramos@gmail.com>
2021-06-09 00:06:41 +0100

    new eval script

--- src/evaluation_script.py
+++ src/evaluation_script.py
@@ -199,23 +199,11 @@ for eval_file in [
 
 
     subprocess.run(["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file,
-                        "--decodying_type=beam_wt_refinement", "--n_beam=4", "--min_len=3", "--rep_window=2"])
+                        "--decodying_type=beam_wt_refinement", "--n_beam=3", "--min_len=3", "--rep_window=2"])
 
     subprocess.run(["python2", "src/test_scores_coco.py", "@experiments/conf_files/" + eval_file,
-                    "--decodying_type=beam_wt_refinement", "--n_beam=4", "--min_len=3", "--rep_window=2"])
+                    "--decodying_type=beam_wt_refinement", "--n_beam=3", "--min_len=3", "--rep_window=2"])
 
-
-    subprocess.run(["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file,
-                        "--decodying_type=beam_wt_refinement", "--n_beam=4", "--min_len=4", "--rep_window=2"])
-
-    subprocess.run(["python2", "src/test_scores_coco.py", "@experiments/conf_files/" + eval_file,
-                    "--decodying_type=beam_wt_refinement", "--n_beam=4", "--min_len=4", "--rep_window=2"])
-
-    subprocess.run(["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file,
-                        "--decodying_type=beam_wt_refinement", "--n_beam=4", "--min_len=5", "--rep_window=2"])
-
-    subprocess.run(["python2", "src/test_scores_coco.py", "@experiments/conf_files/" + eval_file,
-                    "--decodying_type=beam_wt_refinement", "--n_beam=4", "--min_len=5", "--rep_window=2"])
 
    
 
