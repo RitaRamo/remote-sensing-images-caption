c06b1709079dd108773316cb33154118d56aca4d
RitaRamos <rita.mparada.ramos@gmail.com>
2021-06-08 23:26:43 +0100

    new eval script

--- src/evaluation_script.py
+++ src/evaluation_script.py
@@ -211,6 +211,13 @@ for eval_file in [
                     "--decodying_type=beam_wt_refinement", "--n_beam=6", "--min_len=6", "--rep_window=2"])
 
 
+    subprocess.run(["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file,
+                        "--decodying_type=beam_wt_refinement", "--n_beam=6", "--min_len=3", "--rep_window=2"])
+
+    subprocess.run(["python2", "src/test_scores_coco.py", "@experiments/conf_files/" + eval_file,
+                    "--decodying_type=beam_wt_refinement", "--n_beam=6", "--min_len=3", "--rep_window=2"])
+
+
 
 
     # subprocess.run(["python3", "src/test_generate_sentences.py", "@experiments/conf_files/" + eval_file,
